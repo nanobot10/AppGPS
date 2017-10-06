313310b7a31f3e6bcd250d552e6454a2d33fd1f8
nanobot10 <c.alejandro17@gmail.com>
2017-10-06 17:22:16 -0600

    Conexión Bluetooth

--- app/src/main/java/appgps/com/appgps/MainActivity.java
+++ app/src/main/java/appgps/com/appgps/MainActivity.java
@@ -15,6 +15,8 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
@@ -30,7 +32,9 @@ import android.widget.Toast;
 
 import java.io.IOError;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InterruptedIOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
@@ -47,29 +51,140 @@ import android.os.Bundle;
 public class MainActivity extends Activity {
 
     LocationManager locationManager;
-    double longitudeBest, latitudeBest;
     double longitudeGPS, latitudeGPS;
-    double longitudeNetwork, latitudeNetwork;
-    TextView longitudeValueBest, latitudeValueBest;
     TextView longitudeValueGPS, latitudeValueGPS;
-    TextView longitudeValueNetwork, latitudeValueNetwork;
-    ListView listView;
-    List<String> devices = new ArrayList<String>();
+    TextView txtCantidadCO;
+    Button btnCO;
+
+
+
+
+    Handler bluetoothIn;
+    final int handlerState = 0;        				 //used to identify handler message
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private StringBuilder recDataString = new StringBuilder();
+
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static String address = null;
+
+
+    private ConnectedThread mConnectedThread;
+
+
 
     @Override
     public void onCreate(Bundle savedInsctanceState) {
         super.onCreate(savedInsctanceState);
         setContentView(R.layout.activity_main);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        longitudeValueBest = (TextView) findViewById(R.id.longitudeValueBest);
-        latitudeValueBest = (TextView) findViewById(R.id.latitudeValueBest);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         longitudeValueGPS = (TextView) findViewById(R.id.longitudeValueGPS);
         latitudeValueGPS = (TextView) findViewById(R.id.latitudeValueGPS);
-        longitudeValueNetwork = (TextView) findViewById(R.id.longitudeValueNetwork);
-        latitudeValueNetwork = (TextView) findViewById(R.id.latitudeValueNetwork);
-        listView = (ListView) findViewById(R.id.listView);
-        devices.clear();
+        txtCantidadCO = (TextView) findViewById(R.id.txtCantidadCO);
+        btnCO = (Button) findViewById(R.id.btnCO);
 
+        bluetoothIn = new Handler(){
+
+            public void handleMessage(Message msg){
+                if(msg.what == handlerState){
+                    String readMessage = (String) msg.obj;
+                    recDataString.append(readMessage);
+
+                    txtCantidadCO.setText(recDataString.toString());
+                }
+            }
+
+        };
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBTState();
+
+        btnCO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConnectedThread.write("1");
+                Toast.makeText(getBaseContext(), "Se envío señal para verificar conexión", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+
+        return  device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        //creates secure outgoing connecetion with BT device using UUID
+    }
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //Get MAC address from DeviceListActivity via intent
+        Intent intent = getIntent();
+
+        //Get the MAC address from the DeviceListActivty via EXTRA
+        address = intent.getStringExtra(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+
+        //create device and set the MAC address
+        //Log.i("ramiro", "adress : " + address);
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "La creacción del Socket fallo", Toast.LENGTH_LONG).show();
+        }
+        // Establish the Bluetooth socket connection.
+        try
+        {
+            btSocket.connect();
+        } catch (IOException e) {
+            try
+            {
+                btSocket.close();
+            } catch (IOException e2)
+            {
+                //insert code to deal with this
+            }
+        }
+        mConnectedThread = new ConnectedThread(btSocket);
+        mConnectedThread.start();
+
+        //I send a character when resuming.beginning transmission to check device is connected
+        //If it is not an exception will be thrown in the write method and finish() will be called
+        mConnectedThread.write("1");
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        try
+        {
+            //Don't leave Bluetooth sockets open when leaving activity
+            btSocket.close();
+        } catch (IOException e2) {
+            //insert code to deal with this
+        }
+    }
+
+
+    //Checks that the Android device Bluetooth is available and prompts to be turned on if off
+    private void checkBTState() {
+
+        if(btAdapter==null) {
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
     }
 
     private boolean checkLocation(){
@@ -115,110 +230,11 @@ public class MainActivity extends Activity {
 
             }
             locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER, 1000, 10, locationListenerGPS);
+                    LocationManager.NETWORK_PROVIDER, 0, 0, locationListenerGPS);
             button.setText(R.string.pause);
         }
     }
 
-    public void toggleBestUpdates(View view) {
-        if (!checkLocation())
-            return;
-        Button button = (Button) view;
-        if (button.getText().equals(getResources().getString(R.string.pause))) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            }
-            locationManager.removeUpdates(locationListenerBest);
-            button.setText(R.string.resume);
-        } else {
-            Criteria criteria = new Criteria();
-            criteria.setAccuracy(Criteria.ACCURACY_FINE);
-            criteria.setAltitudeRequired(false);
-            criteria.setBearingRequired(false);
-            criteria.setCostAllowed(true);
-            criteria.setPowerRequirement(Criteria.POWER_LOW);
-            String provider = locationManager.getBestProvider(criteria, true);
-            if (provider != null) {
-                locationManager.requestLocationUpdates(provider,1000, 10, locationListenerBest);
-                button.setText(R.string.pause);
-                Toast.makeText(this, "Best Provider is " + provider, Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    public void toggleNetworkUpdates(View view) {
-        if (!checkLocation())
-            return;
-        Button button = (Button) view;
-        if (button.getText().equals(getResources().getString(R.string.pause))) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            }
-            locationManager.removeUpdates(locationListenerNetwork);
-            button.setText(R.string.resume);
-        }
-        else {
-            locationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER, 1000, 10, locationListenerNetwork);
-            Toast.makeText(this, "Network provider started running", Toast.LENGTH_LONG).show();
-            button.setText(R.string.pause);
-        }
-    }
-
-    private final LocationListener locationListenerBest = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            longitudeBest = location.getLongitude();
-            latitudeBest = location.getLatitude();
-
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    longitudeValueBest.setText(longitudeBest + "");
-                    latitudeValueBest.setText(latitudeBest + "");
-
-                }
-            });
-        }
-
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-        }
-    };
-
-    private final LocationListener locationListenerNetwork = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            longitudeNetwork = location.getLongitude();
-            latitudeNetwork = location.getLatitude();
-
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    longitudeValueNetwork.setText(longitudeNetwork + "");
-                    latitudeValueNetwork.setText(latitudeNetwork + "");
-
-                }
-            });
-        }
-
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-
-        }
-        @Override
-        public void onProviderDisabled(String s) {
-
-        }
-    };
 
     private final LocationListener locationListenerGPS = new LocationListener() {
         public void onLocationChanged(Location location) {
@@ -246,116 +262,59 @@ public class MainActivity extends Activity {
     };
 
 
-    public void bluetoothConnect(View view){
-
-        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-        if(adapter == null){
-            Toast.makeText(MainActivity.this,"El dispositivo no soporta Bluetooth",Toast.LENGTH_SHORT).show();
-        }
-
-        if(!adapter.isEnabled()){
-            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableIntent,1);
-        }
-
-        //Consulta de dispositivos sincronizados
-        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
-
-        if(pairedDevices.size()>0){
-
-            for(BluetoothDevice device : pairedDevices){
-                devices.add(device.getName());
-            }
-
-            final StableArrayAdapter stableArrayAdapter = new StableArrayAdapter(this,android.R.layout.simple_list_item_1,devices);
-            listView.setAdapter(stableArrayAdapter);
-
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-            {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view,
-                                        int position, long id) {
-                    // TODO Auto-generated method stub
-                    if(devices.get(position)!=null){
-                        Toast.makeText(MainActivity.this, devices.get(position), Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
 
 
-        }
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
 
+        //creation of the connect thread
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
 
-
-
-    }
-
-
-    private class AcceptThread extends Thread{
-
-        private final BluetoothServerSocket mmServerSocket;
-
-        public AcceptThread(BluetoothAdapter adapter){
-            BluetoothServerSocket tmp = null;
             try {
-                tmp = adapter.listenUsingRfcommWithServiceRecord("APPGPS", UUID.randomUUID());
-            }catch (IOException e){
-
-            }
-
-            mmServerSocket = tmp;
-        }
-
-        public void run(){
-            BluetoothSocket socket = null;
-            Toast.makeText(MainActivity.this,"Esperando Conexión Dispositivos",Toast.LENGTH_SHORT).show();
-            while(true){
-                try{
-                    socket = mmServerSocket.accept();
-                }catch(IOException e){
-                    break;
-                }
-
-                if(socket!=null){
-                    Toast.makeText(MainActivity.this,"Dispositivo Conectado",Toast.LENGTH_SHORT).show();
-                    break;
-                }
-            }
-        }
-
-        /** Will cancel the listening socket, and cause the thread to finish */
-        public void cancel() {
-            try {
-                mmServerSocket.close();
+                //Create I/O streams for connection
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
             } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
         }
-    }
 
 
-    private class StableArrayAdapter extends ArrayAdapter<String> {
+        public void run() {
+            byte[] buffer = new byte[256];
+            int bytes;
 
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-        public StableArrayAdapter(Context context, int textViewResourceId,
-                                  List<String> objects) {
-            super(context, textViewResourceId, objects);
-            for (int i = 0; i < objects.size(); ++i) {
-                mIdMap.put(objects.get(i), i);
+            // Keep looping to listen for received messages
+            while (true) {
+                try {
+                    bytes = mmInStream.read(buffer);        	//read bytes from input buffer
+                    String readMessage = new String(buffer, 0, bytes);
+                    // Send the obtained bytes to the UI Activity via handler
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
             }
         }
+        //write method
+        public void write(String input) {
+            byte[] msgBuffer = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(msgBuffer);                //write bytes over BT connection via outstream
+            } catch (IOException e) {
+                //if you cannot write, close the application
+                Toast.makeText(getBaseContext(), "La Conexión fallo", Toast.LENGTH_LONG).show();
+                finish();
 
-        @Override
-        public long getItemId(int position) {
-            String item = getItem(position);
-            return mIdMap.get(item);
+            }
         }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
     }
+
+
 
 
 }
